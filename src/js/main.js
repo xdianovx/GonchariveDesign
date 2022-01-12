588bae1b61f8f45ad1c3e84a22a56041c3f2354b
Евгений Дианов <dianov.js@gmail.com>
2022-01-12 21:16:39 +0300

    initital

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+import 'focus-visible'
+
+const burgerBtn = document.querySelector('.burger')
+const headerMenu = document.querySelector('.menu')
+
+import Swiper, { Navigation, Pagination } from 'swiper'
+Swiper.use([Navigation, Pagination])
+burgerBtn.addEventListener('click', () => {
+  headerMenu.classList.toggle('active')
+})
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+})
